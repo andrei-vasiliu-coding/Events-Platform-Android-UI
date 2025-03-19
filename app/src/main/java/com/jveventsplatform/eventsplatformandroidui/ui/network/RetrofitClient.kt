b9acdcdc76a3335915854611feb804f268d71057
andrei-vasiliu-coding <andrei-vasiliu@outlook.com>
2025-03-19 12:41:40 +0000

    feat: added notes about connecting to the API when running app on physical device

--- app/src/main/java/com/jveventsplatform/eventsplatformandroidui/ui/network/RetrofitClient.kt
+++ app/src/main/java/com/jveventsplatform/eventsplatformandroidui/ui/network/RetrofitClient.kt
@@ -4,7 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/" // Use correct API URL
+    private const val BASE_URL = "http://10.0.2.2:8080/" /* Use correct API URL; when working on
+    phone, connect to same wifi and use the ip of that wifi instead of 10.0.2.2 */
 
     val apiService: ApiService by lazy {
         Retrofit.Builder()
